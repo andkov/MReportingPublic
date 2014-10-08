e0b7ab9b40aba6d98fc1e3af674cbe9d00e2dcc0
Will Beasley <wibeasley@hotmail.com>
2014-10-08 16:10:20 -0500

    experimenting with file structure

--- produceReports.R
+++ produceReports.R
@@ -1,7 +1,8 @@
+rm(list=ls(all=TRUE))
 
 ########## Production of reports from .Rmd files ###
 #./Vignettes/Questions
-path_index<- base::file.path("./index.Rmd")
+path_index <- base::file.path("./index.Rmd")
 # path_ghPages<- base::file.path("./gh-pages-setup.Rmd")
 # path_toolbox<- base::file.path("./toolbox-install.Rmd")
 # path_TipsTricks <- base::file.path("./tips-and-tricks.Rmd")
@@ -11,29 +12,64 @@ path_index<- base::file.path("./index.Rmd")
 # path_19_09_2014 <- base::file.path("./19-09-2014.Rmd")
 # path_26_09_2014 <- base::file.path("./26-09-2014.Rmd")
 
+# pathMDFilesFunding <- list.files("./funding", full.names=TRUE, recursive=TRUE)
+# pathMDFiles <- c(pathMDFilesFunding)
+
+patternToBuild <- "(?<!README).(R){0,1}md$" #Gets all 'Rmd' and 'md' files, that aren't READMEs.
+pathFilesToBuild <- list.files(full.names=TRUE, recursive=TRUE)
+pathFilesToBuild <- grep(patternToBuild, pathFilesToBuild, perl=TRUE, value=TRUE)
+
+# pathFilesToBuild
+
 # #  Define groups of reports 
 # labLogs <- c(path_26_09_2014)
-allReports<- c(path_index) #path_ghPages,path_toolbox, path_TipsTricks)
+# pathRmdFiles <- c(path_index) #path_ghPages,path_toolbox, path_TipsTricks)
 # Place report paths HERE ###########
-buildthese <- c(allReports) ##########
+# buildthese <- c(allReports) ##########
 # buildthese <- c( labLogs)
 
 ####################################
 
-testit::assert("The knitr Rmd files should exist.", base::file.exists(buildthese))
-# Build the reports
-for( pathRmd in buildthese ) {
+testit::assert("The knitr Rmd files should exist.", base::file.exists(pathFilesToBuild))
+for( pathFile in pathFilesToBuild ) {
   #   pathMd <- base::gsub(pattern=".Rmd$", replacement=".md", x=pathRmd)
-  rmarkdown::render(input = pathRmd, 
+  rmarkdown::render(input = pathFile, 
                     output_format=c(
                       #                        "pdf_document"
                       #                       ,"word_document"
                       #                       ,"md_document"
                       "html_document"
                     ),
-#                     output_dir = "./" ,
-                    #                     output_file=pathMd,
                     clean=TRUE)
 }
-base::system("bundle exec jekyll build")
+
+# testit::assert("The knitr Rmd files should exist.", base::file.exists(pathRmdFiles))
+# # Build the reports
+# for( pathRmd in pathRmdFiles ) {
+#   #   pathMd <- base::gsub(pattern=".Rmd$", replacement=".md", x=pathRmd)
+#   rmarkdown::render(input = pathRmd, 
+#                     output_format=c(
+#                       #                        "pdf_document"
+#                       #                       ,"word_document"
+#                       #                       ,"md_document"
+#                       "html_document"
+#                     ),
+#                     clean=TRUE)
+# }
+
+# testit::assert("The knitr Rmd files should exist.", base::file.exists(pathMDFiles))
+# for( pathMD in pathMDFiles ) {
+#   #   pathMd <- base::gsub(pattern=".Rmd$", replacement=".md", x=pathRmd)
+#   rmarkdown::render(input = pathMD, 
+#                     output_format=c(
+#                       #                        "pdf_document"
+#                       #                       ,"word_document"
+#                       #                       ,"md_document"
+#                       "html_document"
+#                     ),
+#                     clean=TRUE)
+# }
+
+
+# base::system("bundle exec jekyll build")
 # base::system("bundle exec jekyll serve")
